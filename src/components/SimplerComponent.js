a74ce50bfa1c571322f3e91a91e3be93e1116f3c
AcnologiatheEnd <18.chung.winston@ctkprep.org>
2020-12-09 00:40:45 -0500

    Done

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,7 @@
 // Code SimplerComponent Here
+import React from "react"
+
+const SimplerComponent = (props) => <div onClick={printHi(props.handleClick)}>I am just happy</div>
+
+const printHi = (props) => <div>{props}</div>
+export default SimplerComponent
