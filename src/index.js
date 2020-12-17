f7dc5b51797de2ec243af84e83c26bc691c3903d
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-12-17 00:45:31 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log("Happiness is rare")} />
   </div>,
   document.getElementById('root')
 );
